78f38d0a3f1bc10c33710e5c5f258083761c9060
ehawkvu <64124929+ehawkvu@users.noreply.github.com>
2021-10-19 14:01:43 -0500

    Update stage1-exploratory-analysis.py
    stage1 now generates boxplots and histplots for each pair of variables and saves them to pngs

--- code/stage1-exploratory-analysis.py
+++ code/stage1-exploratory-analysis.py
@@ -14,6 +14,17 @@ df.describe(percentiles=[.05, .10, .25, .50, .75, .90, .95])
 print(df.corr())
 
 fig = plt.figure(figsize=(15,15))
-sns.heatmap(df.corr(), vmin=-1, vmax=1, annot=True, cmap='plasma')
+heatmap = sns.heatmap(df.corr(), vmin=-1, vmax=1, annot=True, cmap='plasma')
+heatmap.fig.suptitle("Correlation Heatmap")
+plt.savefig("correlation_heatmap.png", dpi=200)
 
-# TODO: write loop that generates pngs of histograms/boxplots/... of each pair of variables
+for i_column in df.columns:
+  for j_column in df.columns.drop([i_column]):
+    fig = plt.figure(figsize=(8, 8))
+    bp = sns.boxplot(data=df, x=i_column, y=j_column)
+    bp.fig.suptitle(f"{i_column} vs {j_column}")
+    plt.savefig(f"{i_column}_vs_{j_column}_boxplot.png")
+    hp = sns.histplot(data=df, x=i_column, y=j_column)
+    bp.fig.suptitle(f"{i_column} vs {j_column}")
+    plt.savefig(f"{i_column}_vs_{j_column}_histplot.png")
+
