76814091a0f4ca5585397a60f9c1d0ff078ac179
ehawkvu <64124929+ehawkvu@users.noreply.github.com>
2021-11-16 14:05:57 -0600

    Update stage2-project-model.py

--- code/stage2-project-model.py
+++ code/stage2-project-model.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier, export_graphviz
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, confusion_matrix
 
 df = pd.read_csv("../data/student_por_mod.csv")
 
@@ -20,3 +22,23 @@ x = pd.get_dummies(df[predictors], drop_first = True)
 print(x.head())
    
 y = df['failures']
+
+x_train, x_val, y_train, y_val = train_test_split(X, y, test_size = 0.2, random_state=123, stratify = y)   
+
+   
+# First Model: Decision Tree
+   
+d_tree = DecisionTreeClassifier(random_state=0, max_depth=3)
+d_tree.fit(x_train, y_train)
+y_pred = d_tree.predict(x_val)
+print(accuracy_score(y_val, y_pred))
+
+y_pred = d_tree.predict_proba(x_val)[:,1]
+
+labels = np.unique(y_val)
+cm = confusion_matrix(y_val, y_pred > 0.2, labels=labels)
+print(pd.DataFrame(cm, index=labels, columns=labels))
+   
+for func in [recall_score, precision_score, f1_score]:
+   print(f"{func} :  {func(y_val, y_pred > 0.2, average = 'weighted')}")
+
