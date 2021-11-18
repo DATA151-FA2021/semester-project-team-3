03a6af6e4f86ada8fa47633c881a55e5ee374ed8
Ethan <ethan.hawk@valpo.edu>
2021-11-18 14:23:44 -0600

    stage2: random forest now uses runModel

--- code/stage2-project-model.py
+++ code/stage2-project-model.py
@@ -48,12 +48,11 @@ x_train, x_val, y_train, y_val = train_test_split(
 def runModel(model, xt, yt, xv, yv):
     model.fit(xt, yt)
     y_pred = model.predict(xv)
-    print(accuracy_score(yv, y_pred))
     y_pred = model.predict_proba(xv)[:, 1]
     labels = np.unique(yv)
     cm = confusion_matrix(yv, y_pred > 0.2, labels=labels)
     print(pd.DataFrame(cm, index=labels, columns=labels))
-    for func in [recall_score, precision_score, f1_score]:
+    for func in [accuracy_score, recall_score, precision_score, f1_score]:
         print(f"{func} :  {func(yv, y_pred > 0.2, average = 'weighted')}")
 
 
@@ -62,32 +61,10 @@ def runModel(model, xt, yt, xv, yv):
 d_tree = DecisionTreeClassifier(random_state=0, max_depth=3)
 runModel(d_tree, x_train, y_train, x_val, y_val)
 
-'''
-d_tree.fit(x_train, y_train)
-y_pred = d_tree.predict(x_val)
-print(accuracy_score(y_val, y_pred))
-
-y_pred = d_tree.predict_proba(x_val)[:, 1]
-
-labels = np.unique(y_val)
-cm = confusion_matrix(y_val, y_pred > 0.2, labels=labels)
-print(pd.DataFrame(cm, index=labels, columns=labels))
-
-for func in [recall_score, precision_score, f1_score]:
-    print(f"{func} :  {func(y_val, y_pred > 0.2, average = 'weighted')}")
-'''
 # Second Model: Random Forest
 
 r_forest = RandomForestClassifier(n_estimators=500, random_state=7)
-r_forest.fit(x_train, y_train)
-y_pred = r_forest.predict_proba(x_val)[:, 1]
-
-labels = np.unique(y_val)
-cm = confusion_matrix(y_val, y_pred > 0.2, labels=labels)
-print(pd.DataFrame(cm, index=labels, columns=labels))
-
-for func in [accuracy_score, recall_score, precision_score, f1_score]:
-    print(f"{func} : {func(y_val, y_pred > 0.2, average = 'weighted')}")
+runModel(r_forest, x_train, y_train, x_val, y_val)
 
 # Code above runs, have not yet refactored the rest of the doc.
 
